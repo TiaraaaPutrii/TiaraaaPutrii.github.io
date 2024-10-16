c4d3dc297e84b27381311efa8039ab9f9523da16
Tiara Kasma Wati Putri <162066778+TiaraaaPutrii@users.noreply.github.com>
2024-10-16 22:39:01 +0800

    Add files via upload

---
+++ posttest6/dbloginregis.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Okt 2024 pada 08.24
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbloginregis`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Username` varchar(200) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`Id`, `Username`, `Email`, `Age`, `Password`, `gambar`) VALUES
+(34, '123', '123', 123, '123', '2024-10-16.png');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ posttest6/php/register.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylelogin.css">
+    <title>Register</title>
+</head>
+<body>
+      <div class="container">
+        <div class="box form-box">
+
+        <?php 
+         
+         include("config.php");
+         if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $age = $_POST['age'];
+            $password = $_POST['password'];
+
+            $tmp_name = $_FILES['gambar']['tmp_name'];
+            $file_name = $_FILES['gambar']['name'];
+            $file_size = $_FILES['gambar']['size'];
+
+           
+            $max_size = 1 * 1024 * 1024; // 1MB
+
+            $date = date("Y-m-d");
+            $ekstensi = explode('.', $file_name);
+            $ekstensi = strtolower(end($ekstensi));
+
+            $newFileName = sprintf("%s.%s", $date, $ekstensi);
+
+            if ($file_size > $max_size) {
+                echo "<div class='message'>
+                          <p>File terlalu besar. Maksimal ukuran file adalah 1MB.</p>
+                      </div>";
+            } else {
+                
+                move_uploaded_file($tmp_name, '../img/'.$newFileName);
+            
+                $verify_query = mysqli_query($con,"SELECT Email FROM users WHERE Email='$email'");
+
+                if(mysqli_num_rows($verify_query) != 0) {
+                    echo "<div class='message'>
+                              <p>This email is used, Try another One Please!</p>
+                          </div> <br>";
+                    echo "<a href='javascript:self.history.back()'><button class='btn'>Go Back</button>";
+                } else {
+                    
+                    mysqli_query($con,"INSERT INTO users(Username,Email,Age,Password,gambar) VALUES('$username','$email','$age','$password','$newFileName')") or die("Error Occurred");
+
+                    echo "<div class='message'>
+                              <p>Registration successful!</p>
+                          </div> <br>";
+                    echo "<a href='index.php'><button class='btn'>Login Now</button>";
+                }
+            }
+         } else {
+        ?>
+
+            <header>Sign Up</header>
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="field input">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="age">Age</label>
+                    <input type="number" name="age" id="age" autocomplete="off" required>
+                </div>
+                <div class="field input">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" autocomplete="off" required>
+                </div>
+
+                <div class="field input">
+                    <label for="gambar">Upload Gambar (Max 1MB):</label>
+                    <input type="File" name="gambar" accept="image/*" required><br><br>
+                </div>
+
+                <div class="field">
+                    <input type="submit" class="btn" name="submit" value="Register" required>
+                </div>
+                <div class="links">
+                    Already a member? <a href="index.php">Sign In</a>
+                </div>
+            </form>
+
+        <?php } ?>
+        </div>
+      </div>
+</body>
+</html>
